276a0e9ca051343769dc9d5ebf600ac017c45fd1
yamajunn <amiton1115@gmail.com>
2023-11-23 22:51:22 +0900

    daaaaaaaaa

--- Pong.py
+++ Pong.py
@@ -8,15 +8,17 @@ width = 100
 height = 30
 line = "="
 back = " "
-display_array = create_display(width, height, line, back)  # create display
 
-bar_size = 20
+bar_size = 30
 bar_position = width//2 - bar_size//2
 
 break_bool = False
 
+display_array = create_display(width, height, line, back)  # create display
+
 display_array = bar(bar_size, bar_position, display_array, width, height) # bar position
-for i in range(height):
+
+for i in range(height):  # print
     display_array[width*i+width-1] = "\n"
 display_str = "".join(display_array)
 print(display_str)
@@ -26,13 +28,13 @@ print(bar_position)
 while True:
     display_array = create_display(width, height, line, back)  # create display
 
-    bar_position, break_bool = bar_move(bar_position, width, bar_size, break_bool)
+    bar_position, break_bool = bar_move(bar_position, width, bar_size, break_bool)  # bar move
     if break_bool:
         break
 
     display_array = bar(bar_size, bar_position, display_array, width, height) # bar position
 
-    for i in range(height):
+    for i in range(height):  # print
         display_array[width*i+width-1] = "\n"
     display_str = "".join(display_array)
     print(display_str)

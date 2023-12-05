1e59978e65c33256c526ab892a29ef7908cc4aaf
yamajunn <amiton1115@gmail.com>
2023-12-05 11:10:15 +0900

    j

--- Pong.py
+++ Pong.py
@@ -4,6 +4,7 @@ import time
 import readchar
 from timeout_decorator import timeout, TimeoutError
 import random
+import matplotlib.pyplot as plt
 
 from create_display import create_display
 from bar import bar
@@ -45,6 +46,9 @@ print(bar_position)
 
 time_start = 0
 time_end = 0
+
+count = 0
+count_list = []
 while True:
     # if time_end - time_start > 0.14:
     #     time_start = time.time()
@@ -78,10 +82,12 @@ while True:
         else:
             # ball_position[0] -= 1
             # ball_position[1] += 1
-            ball_position = [50, 1]
+            ball_position = [50, 15]
             ball_direction = random.randint(0,3)
             bar_position = width//2 - bar_size//2
             try_count += 1
+            count_list.append(count)
+            count = 0
     elif ball_direction == 3 and ball_position[0] <= width-6 and ball_position[1] <= height-6:
         ball_position[0] += 1
         ball_position[1] += 1
@@ -93,10 +99,12 @@ while True:
         else:
             # ball_position[0] += 1
             # ball_position[1] += 1
-            ball_position = [50, 1]
+            ball_position = [50, 15]
             ball_direction = random.randint(0,3)
             bar_position = width//2 - bar_size//2
             try_count += 1
+            count_list.append(count)
+            count = 0
 
     display_array[ball_position[1]*width+ball_position[0]] = "#"
     display_array[ball_position[1]*width+ball_position[0]+1] = "#"
@@ -127,7 +135,7 @@ while True:
     if 1 <= bar_position+choice and bar_position+choice+bar_size <= width-2:
         bar_position += choice
         # probability_list[ball_direction][choice+1] += (width-width//2) - abs(bar_position - ball_position[0]+bar_size//2)
-    if bar_position <= bar_position <= bar_position+bar_size:
+    if bar_position <= ball_position[0] <= bar_position+bar_size:
         if choice == -1:
             probability_list[ball_direction][0] += 1
         else:
@@ -140,7 +148,28 @@ while True:
     for i in range(height):  # print
         display_array[width*i+width-1] = "\n"
     display_str = "".join(display_array)
+
     print(display_str,try_count)
     print(probability_list)
+
     # print(f"bar_position: {bar_position} ball_position: {ball_position} choice: {choice} ball_direction: {ball_direction} probability: {probability_list[ball_direction]}")
+    
     time.sleep(0.001)
+    count += 1
+    # if len(count_list) == 1000:
+    #     break
+
+x_list = [i for i in range(len(count_list))]
+
+# Figureの初期化
+fig = plt.figure(figsize=(12, 8)) #...1
+
+# Figure内にAxesを追加()
+ax = fig.add_subplot(111) #...2
+ax.plot(x_list, count_list, label="test") #...3
+
+# 凡例の表示
+plt.legend()    
+
+# プロット表示(設定の反映)
+plt.show()
